8f616908468f695ed015b02186b5c1824e66e628
Mathieu Méa <mathieu.mea@gmail.com>
2021-04-12 19:33:20 -0400

    Stuffs

---
+++ README.md
@@ -0,0 +1,2 @@
+# home
+Backup of stuff I use in my Linux home ~/ 🏠🐧
---
+++ gnome-terminal-tabs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# TODO ? -e "htop"
+
+gnome-terminal \
+--tab --working-directory="/home/mmathieum/" \
+--tab --working-directory="/media/NDATA/TO_WATCH" \
+--tab --working-directory="/opt/android-sdk" \
+--tab --working-directory="/media/LDATA/DEV/MT/GIT/mtransitapps" \
+--tab --working-directory="/media/LDATA/DEV/MT" \
+;
