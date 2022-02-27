3e4eef66e6b0e0952dbc9237d51cf3560c9856c1
Mathieu Méa <mathieu.mea@gmail.com>
2022-02-27 16:13:19 -0500

    new scripts

---
+++ inspect_local_drive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo ncdu -x /;
+
+
+# /var/log/journal/*
+# journalctl --disk-usage
+# /etc/systemd/journald.conf
+# sudo journalctl --vacuum-size=512M
+
+# /var/lib/snapd/snaps
+# snap list --all
